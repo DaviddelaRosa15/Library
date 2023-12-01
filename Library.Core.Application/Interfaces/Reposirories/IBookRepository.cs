8f18253ad9f82a7b37234c394d6178d0c465a810
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-12-01 14:49:54 -0400

    Entities repositories

---
+++ Library.Core.Application/Interfaces/Reposirories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library.Core.Application.Interfaces.Repositories;
+using Library.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Application.Interfaces.Reposirories
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+
+    }
+}
---
+++ Library.Infrastructure.Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,21 @@
+﻿using Library.Core.Application.Interfaces.Reposirories;
+using Library.Core.Domain.Entities;
+using Library.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Persistence.Repositories
+{
+    public class AuthorRepository : GenericRepository<Author>, IAuthorRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public AuthorRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
